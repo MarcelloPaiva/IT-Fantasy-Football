c9ef803cb89eac81f56411df6076ccad2151c73e
camdent719 <34377169+camdent719@users.noreply.github.com>
2018-04-26 10:48:06 -0400

    Add files via upload

--- src/Components/Display/Login.js
+++ src/Components/Display/Login.js
@@ -1,20 +1,25 @@
 import React, {Component, Fragment} from 'react'
-import { TextField, Typography, Button, Grid, Paper } from 'material-ui'
+import { Typography, Button } from 'material-ui'
 
-export default props =>
-  <div>
-    <Paper>
-      <Typography variant="headline">User Login</Typography>
-      <form>
-        <TextField
-          id="email"
-          label="Yahoo Email"
-        />
-        <TextField
-          id="password"
-          label="Password"
-          type="password"
-        />
-      </form>
-    </Paper>
-  </div>
+export default class Login extends Component {
+  executeYahooAuthScript() {
+    window.alert("Once we have it up, execute server.js for Yahoo auth");
+  }
+  
+  unsupported() {
+    window.alert("This fantasy provider is not supported at this time.");
+  }
+
+  render() {
+    return (
+      <div>
+        <Typography variant="headline">Login with a Fantasy Football Provider to Add a Team</Typography>
+        <p><Button onClick={() => this.executeYahooAuthScript()}>Add Yahoo Team</Button></p>
+        <p><Button onClick={() => this.unsupported()} style={{color: '#bbbbbb'}}>Add NFL Team</Button></p>
+        <p><Button onClick={() => this.unsupported()} style={{color: '#bbbbbb'}}>Add CBS Team</Button></p>
+        <p><Button onClick={() => this.unsupported()} style={{color: '#bbbbbb'}}>Add ESPN Team</Button></p>     
+      </div>
+    );
+  }
+}
+  
