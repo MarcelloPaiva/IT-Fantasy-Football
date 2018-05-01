31e190bbd35f3bab33df3915172a050d7dc2afc7
muqueca <marcello.paiva@gmail.com>
2018-05-01 10:31:00 -0400

    Add files via upload

--- src/Components/Display/GameCard.js
+++ src/Components/Display/GameCard.js
@@ -1,28 +1,42 @@
 import React, {Component, Fragment} from 'react'
 import { Typography, Button, Grid, Paper } from 'material-ui'
 
-export default props =>
-  <div>
-    <Paper elevation={4}>
-      <Grid container spacing={6}>
-        <Grid item xs={2}>
-          <Typography variant="subheading" color="textSecondary">
-          Team 1
-          </Typography>
-          <Typography variant="headline" >
-          165
-          </Typography>
-        </Grid>
-        <Grid item xs={2}>
-        <Typography variant="subheading" color="textSecondary">
-        Team 2
-        </Typography>
-        <Typography variant="headline" >
-        137.5
-        </Typography>
-        </Grid>
-      </Grid>
-      <Button size="small">See More Detail</Button>
-      <Button size="small">View On Yahoo</Button>
-    </Paper>
-  </div>
+let dataJSON = require('../../Data/sample.json');
+console.log(dataJSON.roster[0]);
+
+export default class GameCard extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = { data: dataJSON };
+  }
+
+  render(){
+    return (
+      <div>
+        <Paper elevation={4}>
+          <Grid container spacing={6}>
+            <Grid item xs={2}>
+              <Typography variant="subheading" color="textSecondary">
+                {this.state.data.team_name}
+              </Typography>
+              <Typography variant="headline" >
+                {this.state.data.matchup.user_score}
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Typography variant="subheading" color="textSecondary">
+                {this.state.data.matchup.opponent_name}
+              </Typography>
+              <Typography variant="headline" >
+                {this.state.data.matchup.opponent_score}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Button size="small">See More Detail</Button>
+          <Button size="small">View On Yahoo</Button>
+        </Paper>
+      </div>
+    );
+  }
+}
