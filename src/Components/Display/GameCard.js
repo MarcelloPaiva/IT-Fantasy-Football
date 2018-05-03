c2570498f15c106842ff0e0dbbb8d35e8f5cd75a
muqueca <marcello.paiva@gmail.com>
2018-05-03 09:38:14 -0400

    Add files via upload

--- src/Components/Display/GameCard.js
+++ src/Components/Display/GameCard.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react'
 import { Typography, Button, Grid, Paper } from 'material-ui'
+import Expanded from './Expanded'
 
 let dataJSON = require('../../Data/sample.json');
 console.log(dataJSON.roster[0]);
@@ -8,34 +9,34 @@ export default class GameCard extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.state = { data: dataJSON };
+    this.state = { data: dataJSON, isExpanded: false };
+    this.showExpanded = this.showExpanded.bind(this);
+  }
+
+  showExpanded(){
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isExpanded
+    }));
+    if (this.state.isExpanded){
+
+    }
   }
 
   render(){
     return (
-      <div>
+      <div class="row center-xs">
+      <div class="box">
         <Paper elevation={4}>
-          <Grid container spacing={6}>
-            <Grid item xs={2}>
-              <Typography variant="subheading" color="textSecondary">
-                {this.state.data.team_name}
-              </Typography>
-              <Typography variant="headline" >
-                {this.state.data.matchup.user_score}
-              </Typography>
-            </Grid>
-            <Grid item xs={2}>
-              <Typography variant="subheading" color="textSecondary">
-                {this.state.data.matchup.opponent_name}
-              </Typography>
-              <Typography variant="headline" >
-                {this.state.data.matchup.opponent_score}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Button size="small">See More Detail</Button>
+            <Typography variant="subheading" color="textSecondary">
+              {this.state.data.team_name}    {this.state.data.matchup.opponent_name}
+            </Typography>
+            <Typography variant="headline" >
+              {this.state.data.matchup.user_score}  |  {this.state.data.matchup.opponent_score}
+            </Typography>
+          <Button onClick={this.showExpanded} size="small">See More Detail</Button>
           <Button size="small">View On Yahoo</Button>
         </Paper>
+      </div>
       </div>
     );
   }
